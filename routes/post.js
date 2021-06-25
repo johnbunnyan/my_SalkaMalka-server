a773befd7c8dded6fa767bed39b33ae154a8bda2
johnbunnyan <jsk717543@gmail.com>
2021-06-25 18:13:23 +0900

    controllers/포스트컨트롤러구현완료

--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController')
 
-router.post('/', postController.seePostController)
-router.get('/{postId}',postController.uploadController)
-router.delete('/{postId}', postController.deletePostController)
-router.put('{postId}/close', postController.closePostController)
+router.post('', postController.uploadController)
+router.get('/:postId',postController.seePostController)
+router.delete('/:postId', postController.deletePostController)
+router.put('/:postId/close', postController.closePostController)
 
 module.exports = router;
